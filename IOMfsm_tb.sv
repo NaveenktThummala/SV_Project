2fc298118d4c9f4d0ac33994de252b98fe3efcbf
kamalapuram09 <163194328+kamalapuram09@users.noreply.github.com>
2024-03-20 13:48:59 -0700

    Update IOMfsm_tb.sv

--- IOMfsm_tb.sv
+++ IOMfsm_tb.sv
@@ -1,67 +1,58 @@
 module top;
 
 bit CLK = '0;
-bit MNMX = '1;
-bit TEST = '1;
-bit RESET = '0;
-bit READY = '1;
-bit NMI = '0;
-bit INTR = '0;
-bit HOLD = '0;
+bit RESET ='0;
+initial
+	begin
 
-wire logic [7:0] AD;
-logic [19:8] A;
-logic HLDA;
-logic IOM;
-logic WR;
-logic RD;
-logic SSO;
-logic INTA;
-logic ALE;
-logic DTR;
-logic DEN;
-
+		in.MNMX = '1;
+		in.TEST = '1;
+		in.READY = '1;
+ 		in.NMI = '0;
+ 		in.INTR = '0;
+ 		in.HOLD = '0;
+	end
 
 logic [19:0] Address;
 wire [7:0]  Data;
 
-logic CS1, CS2;
+logic CS1, CS2, CS3, CS4;
 
-Intel8088 P(CLK, MNMX, TEST, RESET, READY, NMI, INTR, HOLD, AD, A, HLDA, IOM, WR, RD, SSO, INTA, ALE, DTR, DEN);
-IOMFSM #(.IOM(0)) dut1 (CLK,RESET,ALE,RD,WR,CS1,Address,Data);
-IOMFSM #(.IOM(0)) dut2 (CLK,RESET,ALE,RD,WR,CS2,Address,Data);
-IOMFSM #(.IOM(1)) dut3 (CLK,RESET,ALE,RD,WR,CS1,Address,Data);
-IOMFSM #(.IOM(1)) dut4 (CLK,RESET,ALE,RD,WR,CS2,Address,Data);
+
+IOMFSM #(1) dut1 (in.Peripheral, CS1, Address, Data);
+IOMFSM #(1) dut2 (in.Peripheral, CS2, Address, Data);
+IOMFSM #(1) dut3 (in.Peripheral, CS3, Address, Data);
+IOMFSM #(1) dut4 (in.Peripheral, CS4, Address, Data);
+
+Intel8088Pins in(.CLK(CLK), .RESET(RESET));
+Intel8088 P(in.Processor);
 
 always_comb
-begin
-{CS1,CS2} = 2'b00;
-if(!IOM)
-begin 
-	if(!Address[19])
-		{CS1,CS2} = 2'b01;
-	else
-		{CS1,CS2} = 2'b10;
-end
-else
-	if(16'h1C00 <= Address[15:0] <= 16'h1DFF)
-		{CS1,CS2} = 2'b01;
-	else if (16'hFF00 <= Address[15:0] <= 16'hFF0F)
-		{CS1,CS2} = 2'b10;
-	else 
-		{CS1,CS2} = 2'b00;
-end
+	begin
+		{CS1,CS2,CS3,CS4} = 4'b0000;
+		if(~in.IOM && ~Address[19]) 
+			CS1 = 1'b1;
+		else if (~in.IOM && Address[19]) 
+			CS2 = 1'b1;
+		else if(in.IOM && Address[15:9] == 7'hE)
+			CS3 = 1'b1;
 
+		else if(in.IOM && Address[15:4] == 12'hFF0)
+			CS4 = 1'b1;
+
+	end
+	
 // 8282 Latch to latch bus address
 always_latch
 begin
-if (ALE)
-	Address <= {A, AD};
+if (in.ALE)
+	Address <= {in.A, in.AD};
 end
 
 // 8286 transceiver
-assign Data =  (DTR & ~DEN) ? AD   : 'z;
-assign AD   = (~DTR & ~DEN) ? Data : 'z;
+assign Data = (in.DTR & ~in.DEN) ? in.AD : 'z;
+assign in.AD   = (~in.DTR & ~in.DEN) ? Data : 'z;
+
 always #50 CLK = ~CLK;
 
 initial
