a63190c33423b25ba32199bff132bb6f5a641226
NaveenktThummala <163190577+NaveenktThummala@users.noreply.github.com>
2024-03-20 21:52:56 -0700

    Update Intel8088Pins.sv

--- Intel8088Pins.sv
+++ Intel8088Pins.sv
@@ -1,33 +1,4 @@
-interface Intel8088Pins(input CLK, input RESET);
-
-logic MNMX;
-logic TEST;
-logic READY;
-logic NMI;
-logic INTR;
-logic HOLD;
-tri [7:0]AD;
-tri [19:8]A;
-logic HLDA;
-logic IOM;
-logic WR;
-//logic CS;
-logic RD;
-logic SSO;
-logic INTA;
-logic ALE;			
-logic DTR;
-logic DEN;
-logic [7:0]Data;
-
-
-modport Processor (input CLK, MNMX, TEST, RESET, NMI, INTR, HOLD, READY,
-					output A, HLDA, IOM, WR, RD, SSO, INTA, ALE, DTR, DEN, 
-					inout AD);
-
-modport Peripheral (input CLK, RESET, WR, RD, ALE);
-
-endinterfaceinterface Intel8088Pins(input CLK, input RESET);
+Intel8088Pins(input CLK, input RESET);
 
 logic MNMX;
 logic TEST;
@@ -49,12 +20,11 @@ logic DEN;
 tri [7:0]Data;
 logic [19:0] Address;
 
+modport Processor (input CLK, MNMX, TEST, RESET, NMI, INTR, HOLD, READY,HLDA ,
+			output A,  IOM, WR, RD, SSO, INTA, ALE, DTR, DEN, 
+			inout AD);
 
-	modport Processor (input CLK, MNMX, TEST, RESET, NMI, INTR, HOLD, READY,HLDA ,
-					output A,  IOM, WR, RD, SSO, INTA, ALE, DTR, DEN, 
-					inout AD);
-
-	modport Peripheral (input CLK, RESET, WR, RD, ALE,Address,
+modport Peripheral (input CLK, RESET, WR, RD, ALE,Address,
 			   	inout Data);
 
 endinterface
